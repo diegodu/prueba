eee207b172f3bbe1c1db2a01900b7e7935fd769b
Diego Duchimaza <dduchimazad@est.ups.edu.ec>
2019-07-28 22:54:13 -0500

    facturacion

--- src/facturacion/Facturacion.java
+++ src/facturacion/Facturacion.java
@@ -11,7 +11,7 @@ package facturacion;
 public class Facturacion {
 
     /**
-     * @param args the command line arguments
+     * @param args the command line argument
      */
    
     public static void main(String[] args) {
